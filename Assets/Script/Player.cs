b795c323927f02320aad223d3456aefca0996d66
tahuynhduc <tahduc03032000@gmail.com>
2023-03-28 16:37:52 +0700

    di chuyên play và obstacle

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && transform.position.y <= -4.5)
+        {
+            rb.velocity = Vector3.up*moveSpeed;
+        }
+    }
+}
+
+
