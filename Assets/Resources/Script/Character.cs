dec6d82a751b68a1d2d9334068625dad278a6586
tahuynhduc <tahduc03032000@gmail.com>
2023-05-03 17:18:58 +0700

    update state character

--- Assets/Resources/Script/Character.cs
+++ Assets/Resources/Script/Character.cs
@@ -4,16 +4,151 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public int check;
-    // Start is called before the first frame update
+    int check;
+    [SerializeField] GameObject SelectCharacterOne;
+    [SerializeField] GameObject SelectedCharacterOne;
+    [SerializeField] GameObject SelectCharacterTwo;
+    [SerializeField] GameObject SelectedCharacterTwo;
+    [SerializeField] GameObject SelectCharacterThree;
+    [SerializeField] GameObject SelectedCharacterThree;
+    [SerializeField] GameObject SelectCharacterFor;
+    [SerializeField] GameObject SelectedCharacterFor;
+    [SerializeField] GameObject SelectCharacterFive;
+    [SerializeField] GameObject SelectedCharacterFive;
+    [SerializeField] GameObject SelectCharacterSix;
+    [SerializeField] GameObject SelectedCharacterSix;
+    [SerializeField] GameObject SelectCharacterSeven;
+    [SerializeField] GameObject SelectedCharacterSeven;
     void Start()
     {
-        
+        check = PlayerPrefs.GetInt("checkCharacter",1);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (check == 1)
+        {
+            SelectCharacterOne.SetActive(false);
+            SelectedCharacterOne.SetActive(true);
+            SelectCharacterTwo.SetActive(true);
+            SelectedCharacterTwo.SetActive(false);
+            SelectCharacterThree.SetActive(true);
+            SelectedCharacterThree.SetActive(false);
+            SelectCharacterFor.SetActive(true);
+            SelectedCharacterFor.SetActive(false);
+            SelectCharacterFive.SetActive(true);
+            SelectedCharacterFive.SetActive(false);
+            SelectCharacterSix.SetActive(true);
+            SelectedCharacterSix.SetActive(false);
+            SelectCharacterSeven.SetActive(true);
+            SelectedCharacterSeven.SetActive(false);
+        }
+        if (check == 2)
+        {
+            SelectCharacterOne.SetActive(true);
+            SelectedCharacterOne.SetActive(false);
+            SelectCharacterTwo.SetActive(false);
+            SelectedCharacterTwo.SetActive(true);
+            SelectCharacterThree.SetActive(true);
+            SelectedCharacterThree.SetActive(false);
+            SelectCharacterFor.SetActive(true);
+            SelectedCharacterFor.SetActive(false);
+            SelectCharacterFive.SetActive(true);
+            SelectedCharacterFive.SetActive(false);
+            SelectCharacterSix.SetActive(true);
+            SelectedCharacterSix.SetActive(false);
+            SelectCharacterSeven.SetActive(true);
+            SelectedCharacterSeven.SetActive(false);
+        }
+        if (check == 3)
+        {
+            SelectCharacterOne.SetActive(true);
+            SelectedCharacterOne.SetActive(false);
+            SelectCharacterTwo.SetActive(true);
+            SelectedCharacterTwo.SetActive(false);
+            SelectCharacterThree.SetActive(false);
+            SelectedCharacterThree.SetActive(true);
+            SelectCharacterFor.SetActive(true);
+            SelectedCharacterFor.SetActive(false);
+            SelectCharacterFive.SetActive(true);
+            SelectedCharacterFive.SetActive(false);
+            SelectCharacterSix.SetActive(true);
+            SelectedCharacterSix.SetActive(false);
+            SelectCharacterSeven.SetActive(true);
+            SelectedCharacterSeven.SetActive(false);
+        }
+        if (check == 4)
+        {
+            SelectCharacterOne.SetActive(true);
+            SelectedCharacterOne.SetActive(false);
+            SelectCharacterTwo.SetActive(true);
+            SelectedCharacterTwo.SetActive(false);
+            SelectCharacterThree.SetActive(true);
+            SelectedCharacterThree.SetActive(false);
+            SelectCharacterFor.SetActive(false);
+            SelectedCharacterFor.SetActive(true);
+            SelectCharacterFive.SetActive(true);
+            SelectedCharacterFive.SetActive(false);
+            SelectCharacterSix.SetActive(true);
+            SelectedCharacterSix.SetActive(false);
+            SelectCharacterSeven.SetActive(true);
+            SelectedCharacterSeven.SetActive(false);
+        }
+        if (check == 5)
+        {
+            SelectCharacterOne.SetActive(true);
+            SelectedCharacterOne.SetActive(false);
+            SelectCharacterTwo.SetActive(true);
+            SelectedCharacterTwo.SetActive(false);
+            SelectCharacterThree.SetActive(true);
+            SelectedCharacterThree.SetActive(false);
+            SelectCharacterFor.SetActive(true);
+            SelectedCharacterFor.SetActive(false);
+            SelectCharacterFive.SetActive(false);
+            SelectedCharacterFive.SetActive(true);
+            SelectCharacterSix.SetActive(true);
+            SelectedCharacterSix.SetActive(false);
+            SelectCharacterSeven.SetActive(true);
+            SelectedCharacterSeven.SetActive(false);
+        }
+        if (check == 6)
+        {
+            SelectCharacterOne.SetActive(true);
+            SelectedCharacterOne.SetActive(false);
+            SelectCharacterTwo.SetActive(true);
+            SelectedCharacterTwo.SetActive(false);
+            SelectCharacterThree.SetActive(true);
+            SelectedCharacterThree.SetActive(false);
+            SelectCharacterFor.SetActive(true);
+            SelectedCharacterFor.SetActive(false);
+            SelectCharacterFive.SetActive(true);
+            SelectedCharacterFive.SetActive(false);
+            SelectCharacterSix.SetActive(false);
+            SelectedCharacterSix.SetActive(true);
+            SelectCharacterSeven.SetActive(true);
+            SelectedCharacterSeven.SetActive(false);
+        }
+        if (check == 7)
+        {
+            SelectCharacterOne.SetActive(true);
+            SelectedCharacterOne.SetActive(false);
+            SelectCharacterTwo.SetActive(true);
+            SelectedCharacterTwo.SetActive(false);
+            SelectCharacterThree.SetActive(true);
+            SelectedCharacterThree.SetActive(false);
+            SelectCharacterFor.SetActive(true);
+            SelectedCharacterFor.SetActive(false);
+            SelectCharacterFive.SetActive(true);
+            SelectedCharacterFive.SetActive(false);
+            SelectCharacterSix.SetActive(true);
+            SelectedCharacterSix.SetActive(false);
+            SelectCharacterSeven.SetActive(false);
+            SelectedCharacterSeven.SetActive(true);
+        }
+    }
+    public void checkCharacter(int value)
+    {
+        check = value;
+        PlayerPrefs.SetInt("checkCharacter",check);
+        PlayerPrefs.Save();
     }
 }
--- Assets/Resources/Script/GameOverData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameOverData", menuName = "Game Data/GameOverData")]
-public class GameOverData : ScriptableObject
-{
-    public int timesOver = 0;
-}
