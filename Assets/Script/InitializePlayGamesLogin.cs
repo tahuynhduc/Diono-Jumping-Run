b522cf2310eecf369a6e40c040e88dbc6a994ba6
huỳnh đức tạ <tahduc03032000@gmail.com>
2023-05-23 17:01:50 +0700

    playerLogin

---
+++ Assets/Script/InitializePlayGamesLogin.cs
@@ -0,0 +1,37 @@
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using Mono.Cecil.Cil;
+
+public class InitializePlayGamesLogin : MonoBehaviour
+{
+    private PlayGamesClientConfiguration config;
+    private void Awake()
+    {
+        ConfigureGPGS();
+        SigIntoGPGS(SignInInteractivity.CanPromptOnce,config);
+    }
+
+    internal void ConfigureGPGS()
+    {
+        config = new PlayGamesClientConfiguration.Builder().Build();
+    }
+    internal void SigIntoGPGS(SignInInteractivity interactivity,PlayGamesClientConfiguration configuration)
+    {
+        configuration = config;
+        PlayGamesPlatform.InitializeInstance(configuration);
+        PlayGamesPlatform.Activate();
+        PlayGamesPlatform.Instance.Authenticate(interactivity,(code) =>
+        {
+            if (code == SignInStatus.Success) 
+            {
+                Debug.Log(code);
+            }
+            else
+            {
+                Debug.Log(code);
+            }
+        });
+    }
+}
